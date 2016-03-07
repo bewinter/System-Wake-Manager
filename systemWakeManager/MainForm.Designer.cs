b498b33bd92508a77c1cec67fc7c2a8b9a902a77
Eric <Omniru@users.noreply.github.com>
2016-03-07 15:48:48 -0700

    Added files via upload

---
+++ systemWakeManager/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 3/7/2016
+ * Time: 1:58 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace systemWakeManager
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox cmdResults;
+		private System.Windows.Forms.Button btnDisableButton;
+		private System.Windows.Forms.ComboBox alterWakeDeviceListCoB;
+		private System.Windows.Forms.Button btnAllWakeDevices;
+		private System.Windows.Forms.Button btnArmedWakeDevices;
+		private System.Windows.Forms.Button btnLastUsedWakeDevice;
+		private System.Windows.Forms.Button btnEnableWakeDevice;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmdResults = new System.Windows.Forms.TextBox();
+			this.btnDisableButton = new System.Windows.Forms.Button();
+			this.alterWakeDeviceListCoB = new System.Windows.Forms.ComboBox();
+			this.btnAllWakeDevices = new System.Windows.Forms.Button();
+			this.btnArmedWakeDevices = new System.Windows.Forms.Button();
+			this.btnLastUsedWakeDevice = new System.Windows.Forms.Button();
+			this.btnEnableWakeDevice = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// cmdResults
+			// 
+			this.cmdResults.Location = new System.Drawing.Point(12, 38);
+			this.cmdResults.Multiline = true;
+			this.cmdResults.Name = "cmdResults";
+			this.cmdResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.cmdResults.Size = new System.Drawing.Size(661, 227);
+			this.cmdResults.TabIndex = 0;
+			// 
+			// btnDisableButton
+			// 
+			this.btnDisableButton.Location = new System.Drawing.Point(540, 326);
+			this.btnDisableButton.Name = "btnDisableButton";
+			this.btnDisableButton.Size = new System.Drawing.Size(133, 23);
+			this.btnDisableButton.TabIndex = 1;
+			this.btnDisableButton.Text = "Disable Wake Device";
+			this.btnDisableButton.UseVisualStyleBackColor = true;
+			this.btnDisableButton.Click += new System.EventHandler(this.DisableButtonClick);
+			// 
+			// alterWakeDeviceListCoB
+			// 
+			this.alterWakeDeviceListCoB.FormattingEnabled = true;
+			this.alterWakeDeviceListCoB.Location = new System.Drawing.Point(12, 326);
+			this.alterWakeDeviceListCoB.Name = "alterWakeDeviceListCoB";
+			this.alterWakeDeviceListCoB.Size = new System.Drawing.Size(383, 21);
+			this.alterWakeDeviceListCoB.Sorted = true;
+			this.alterWakeDeviceListCoB.TabIndex = 2;
+			this.alterWakeDeviceListCoB.SelectedIndexChanged += new System.EventHandler(this.AlterWakeDeviceListCoBSelectedIndexChanged);
+			// 
+			// btnAllWakeDevices
+			// 
+			this.btnAllWakeDevices.Location = new System.Drawing.Point(12, 271);
+			this.btnAllWakeDevices.Name = "btnAllWakeDevices";
+			this.btnAllWakeDevices.Size = new System.Drawing.Size(108, 23);
+			this.btnAllWakeDevices.TabIndex = 3;
+			this.btnAllWakeDevices.Text = "All Wake Devices";
+			this.btnAllWakeDevices.UseVisualStyleBackColor = true;
+			this.btnAllWakeDevices.Click += new System.EventHandler(this.BtnAllWakeDevicesClick);
+			// 
+			// btnArmedWakeDevices
+			// 
+			this.btnArmedWakeDevices.Location = new System.Drawing.Point(126, 271);
+			this.btnArmedWakeDevices.Name = "btnArmedWakeDevices";
+			this.btnArmedWakeDevices.Size = new System.Drawing.Size(125, 23);
+			this.btnArmedWakeDevices.TabIndex = 4;
+			this.btnArmedWakeDevices.Text = "Armed Wake Devices";
+			this.btnArmedWakeDevices.UseVisualStyleBackColor = true;
+			this.btnArmedWakeDevices.Click += new System.EventHandler(this.BtnArmedWakeDevicesClick);
+			// 
+			// btnLastUsedWakeDevice
+			// 
+			this.btnLastUsedWakeDevice.Location = new System.Drawing.Point(257, 271);
+			this.btnLastUsedWakeDevice.Name = "btnLastUsedWakeDevice";
+			this.btnLastUsedWakeDevice.Size = new System.Drawing.Size(138, 23);
+			this.btnLastUsedWakeDevice.TabIndex = 5;
+			this.btnLastUsedWakeDevice.Text = "Last Used Wake Device";
+			this.btnLastUsedWakeDevice.UseVisualStyleBackColor = true;
+			this.btnLastUsedWakeDevice.Click += new System.EventHandler(this.BtnLastUsedWakeDeviceClick);
+			// 
+			// btnEnableWakeDevice
+			// 
+			this.btnEnableWakeDevice.Location = new System.Drawing.Point(401, 326);
+			this.btnEnableWakeDevice.Name = "btnEnableWakeDevice";
+			this.btnEnableWakeDevice.Size = new System.Drawing.Size(133, 23);
+			this.btnEnableWakeDevice.TabIndex = 6;
+			this.btnEnableWakeDevice.Text = "Enable Wake Device";
+			this.btnEnableWakeDevice.UseVisualStyleBackColor = true;
+			this.btnEnableWakeDevice.Click += new System.EventHandler(this.BtnEnableWakeDeviceClick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(661, 23);
+			this.label1.TabIndex = 8;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(688, 364);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnEnableWakeDevice);
+			this.Controls.Add(this.btnLastUsedWakeDevice);
+			this.Controls.Add(this.btnArmedWakeDevices);
+			this.Controls.Add(this.btnAllWakeDevices);
+			this.Controls.Add(this.alterWakeDeviceListCoB);
+			this.Controls.Add(this.btnDisableButton);
+			this.Controls.Add(this.cmdResults);
+			this.Name = "MainForm";
+			this.Text = "System Wake Manager";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ systemWakeManager/MainForm.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 3/7/2016
+ * Time: 1:58 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace systemWakeManager
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string itemToAlter;
+
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+
+		}
+		
+		string runCommand(string command)
+		{
+			try {
+				
+			
+			// create the ProcessStartInfo using "cmd" as the program to be run,
+			// and "/c " as the parameters.
+			// Incidentally, /c tells cmd that we want it to execute the command that follows,
+			// and then exit.
+				
+			//string command = "powercfg -devicequery wake_from_any";
+			         
+			System.Diagnostics.ProcessStartInfo procStartInfo =
+				new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+			
+			// The following commands are needed to redirect the standard output.
+			// This means that it will be redirected to the Process.StandardOutput StreamReader.
+			procStartInfo.RedirectStandardOutput = true;
+			procStartInfo.UseShellExecute = false;
+			// Do not create the black window.
+			procStartInfo.CreateNoWindow = true;
+			// Now we create a process, assign its ProcessStartInfo and start it
+			System.Diagnostics.Process proc = new System.Diagnostics.Process();
+			proc.StartInfo = procStartInfo;			
+			proc.StartInfo.Verb = "runas";
+			
+			proc.Start();
+			// Get the output into a string
+			string result = proc.StandardOutput.ReadToEnd();
+			    
+			return(result);
+			
+			} catch (Exception e) {				
+				MessageBox.Show(e.ToString());				
+				return("");
+			}
+		}
+		
+		void getWakeDevices(string command, bool justClear)
+		{
+			/*// Start the child process.
+			 Process p = new Process();
+			 // Redirect the output stream of the child process.
+			 p.StartInfo.UseShellExecute = false;
+			 p.StartInfo.RedirectStandardOutput = true;
+			 p.StartInfo.FileName = "getAnyWakeDevice.bat";
+			 p.Start();
+			 // Do not wait for the child process to exit before
+			 // reading to the end of its redirected stream.
+			 // p.WaitForExit();
+			 // Read the output stream first and then wait.
+			 string output = p.StandardOutput.ReadToEnd();
+			 p.WaitForExit();	
+			 cmdResults.Text = output;
+			 
+			 string[] lines = output.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+			 
+			 disableWakeDeviceListCoB.Items.Add("");
+			 
+			 foreach (var element in lines) {
+			 	
+			 	if (element != "" && element.Contains(@":\") == false)
+			 	{
+			 		disableWakeDeviceListCoB.Items.Add(element);
+			 	}
+			 }*/
+			 
+			string result = runCommand(command);
+			 	
+			try {
+				
+			 	cmdResults.Text = result;
+			    
+				//result = result.Replace("\r","");
+				string[] lines = result.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+			 
+				//lines.Sort;
+				
+				alterWakeDeviceListCoB.Items.Clear();
+				
+				if (justClear == false) {
+				
+				
+					alterWakeDeviceListCoB.Items.Add("");
+			 
+					foreach (var element in lines) {
+			 	
+						if (element != "" && element.Contains(@":\") == false && element.Contains("NONE") == false) {
+							alterWakeDeviceListCoB.Items.Add(element);
+						}
+			    
+					}
+				}
+			} catch (Exception objException) {
+			 	MessageBox.Show(objException.ToString());
+			}
+			 
+		}
+		void AlterWakeDeviceListCoBSelectedIndexChanged(object sender, EventArgs e)
+		{
+			itemToAlter = alterWakeDeviceListCoB.Text;
+		}
+		void DisableButtonClick(object sender, EventArgs e)
+		{
+			if (itemToAlter != "" && itemToAlter != null) {
+				DialogResult dialogResult = MessageBox.Show("Are you sure you want to disable this item? \n\n" + itemToAlter, itemToAlter, MessageBoxButtons.YesNo);
+				if (dialogResult == DialogResult.Yes) {
+					
+					//runCommand(@"powercfg -devicedisablewake “" + itemToAlter + "”");
+					
+					getWakeDevices(@"powercfg -devicedisablewake """ + itemToAlter + @"""", false);
+					
+					MessageBox.Show("Done");
+					
+					label1.Text = "Armed Wake Devices";
+					getWakeDevices("powercfg -devicequery wake_armed", false);
+					
+				} else if (dialogResult == DialogResult.No) {
+					//do something else
+				}
+			}
+		}
+		void BtnEnableWakeDeviceClick(object sender, EventArgs e)
+		{
+			if (itemToAlter != "" && itemToAlter != null) {
+				DialogResult dialogResult = MessageBox.Show("Are you sure you want to enable this item? \n\n" + itemToAlter, itemToAlter, MessageBoxButtons.YesNo);
+				if (dialogResult == DialogResult.Yes) {
+					
+					getWakeDevices(@"powercfg -deviceenablewake  """ + itemToAlter + @"""", false);
+					//runCommand(@"powercfg -deviceenablewake  “" + itemToAlter + "”");
+					
+					MessageBox.Show("Done");
+					
+					label1.Text = "Armed Wake Devices";
+					getWakeDevices("powercfg -devicequery wake_armed", false);
+					
+				} else if (dialogResult == DialogResult.No) {
+					//do something else
+				}
+			}
+		}
+
+		void BtnAllWakeDevicesClick(object sender, EventArgs e)
+		{
+			label1.Text = "All Wake Devices";
+			getWakeDevices("powercfg -devicequery wake_from_any", false);
+		}
+		void BtnArmedWakeDevicesClick(object sender, EventArgs e)
+		{
+			label1.Text = "Armed Wake Devices";
+			getWakeDevices("powercfg -devicequery wake_armed", false);
+		}
+		void BtnLastUsedWakeDeviceClick(object sender, EventArgs e)
+		{
+			label1.Text = "Last Used Wake Devices";
+			getWakeDevices("powercfg -lastwake", true);
+		}
+
+	}
+}
